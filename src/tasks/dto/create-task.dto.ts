e062fe2e09f1a610ec76b59cffd2ee92091cc837
MelissaPleitez <melissa.ramirez0pleitez@gmail.com>
2026-01-29 17:43:43 -0600

    Add Tasks DTOs

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,25 @@
-export class CreateTaskDto {}
+import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsNotEmpty()
+  category: string;
+
+  @IsEnum(['LOW', 'MEDIUM', 'HEIGH'], { message: 'Priority must be one of the following values: low, medium, high' })
+  priority: string;
+
+  @IsEnum(['PENDING', 'IN_PROGRESS', 'DONE'], { message: 'Status must be one of the following values: pending, in_progress, done' })
+  status: string;
+
+  @IsString()
+  @IsOptional()
+  coverPic?: string;
+}
